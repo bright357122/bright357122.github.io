ee59e2c609df8be52e52fc161d176663b015748d
bright357122 <socool1668@gmail.com>
2016-11-14 23:31:31 +0800

    Add files via upload

---
+++ BrightApp/BrightApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrightApp
+//
+//  Created by user_22 on 2016/11/14.
+//  Copyright © 2016年 ntou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
